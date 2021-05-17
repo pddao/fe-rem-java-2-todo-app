da1cb7643a2b8efa736887460d98b72b626f6d28
d_dao <dung.dao@hotmail.de>
2021-05-17 14:17:15 +0200

    Edit apiservice.js

--- frontend/src/service/apiservice.js
+++ frontend/src/service/apiservice.js
@@ -3,3 +3,21 @@ import axios from "axios";
 function loadTodos() {
   return axios.get("api/todo").then((response) => response.data);
 }
+
+function postNewTodo(newTodo) {
+  return axios.post("api/todo", { newTodo }).then((response) => response.data);
+}
+
+function deleteTodo(todo) {
+  return axios.delete("api/todo/" + todo.id);
+}
+
+function updateTodo(todo) {
+  return axios
+    .put("api/todo" + todo.id, todo)
+    .then((response) => response.data);
+}
+
+function getTodo(id) {
+  return axios.get("api/todo/" + id).then((response) => response.data);
+}
